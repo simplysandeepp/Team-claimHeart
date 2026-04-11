cb42e99435432f44a5bcc804cf45370dbfe50f17
Its-soul <1948immortalsoul@gmail.com>
2026-04-11 18:17:17 +0530

    second commit

---
+++ backend/app/utils/ocr.py
@@ -0,0 +1,11 @@
+
+import easyocr
+
+reader = easyocr.Reader(['en'])
+
+def extract_text_from_image(image_path):
+    results = reader.readtext(image_path, detail=0)
+    
+    text = "\n".join(results)
+    
+    return text
